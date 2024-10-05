12496d462874e154962315f43f2953df4603ae2e
hemkan <hemkano1403@gmail.com>
2024-10-05 11:36:00 -0400

    fix names + add type on form

--- frontend/pages/report.py
+++ frontend/pages/report.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-# Initialize session state variables for form and agent
 if 'form' not in st.session_state:
     st.session_state.form = False
 
@@ -10,11 +9,13 @@ if 'agent' not in st.session_state:
 if 'messages' not in st.session_state:
     st.session_state.messages = []
 
-if st.button("File a Report"):
+st.write("# Report")
+
+if st.button("File a Report Manually"):
     st.session_state.form = True
     st.session_state.agent = False
 
-if st.button("Talk to an Agent"):
+if st.button("Help from an Agent"):
     st.session_state.agent = True
     st.session_state.form = False
 
@@ -31,17 +32,17 @@ if st.session_state.get("agent"):
             st.session_state.messages.append(f"You: {user_message}")
             st.session_state.messages.append(f"Despair: Thank you for your message!")
 
-    
-
 if st.session_state.get("form"):
     st.write("# File a Report")
 
     name = st.text_input("Name")
     address = st.text_input("Address")
+    type = st.selectbox("Type", ["Electricity", "Water", "Gas"])
     description = st.text_area("Description")
     
     if st.button("Submit"):
         st.write("Report submitted successfully")
         st.write(f"Name: {name}")
         st.write(f"Address: {address}")
+        st.write(f"Type: {type}")
         st.write(f"Description: {description}")
