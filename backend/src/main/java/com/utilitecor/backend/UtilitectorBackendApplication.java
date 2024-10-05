c4308fa2de0231ec57f6da3a2d431d7dbfb6b8b0
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2024-10-04 22:13:23 -0400

    Initial Spring Boot project

---
+++ backend/src/main/java/com/utilitecor/backend/UtilitectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.utilitecor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilitectorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilitectorBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/utilitecor/backend/UtilitectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilitecor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitectorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
