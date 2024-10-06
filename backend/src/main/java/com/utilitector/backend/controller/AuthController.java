68fca179b324d30b403b50474c4b252248c3ce40
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2024-10-06 02:14:18 -0400

    Created authentication framework
    username, passwordhash (sha256+base64), token (random 512-bit in base64)

---
+++ backend/src/main/java/com/utilitector/backend/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.utilitector.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.utilitector.backend.request.CredentialRequest;
+import com.utilitector.backend.response.CredentialResponse;
+import com.utilitector.backend.service.AuthService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<CredentialResponse> registerUser(@RequestBody CredentialRequest credReq) {
+        CredentialResponse response = authService.registerUser(credReq);
+        if (response == null) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok(response);
+    }
+    
+}
